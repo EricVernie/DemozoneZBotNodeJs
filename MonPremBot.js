bc3b9f6cdea31eb94f1940350366f1bfeb410389
Eric Vernie <ericv@microsoft.com>
2016-10-25 15:57:34 +0200

    second commit

--- MonPremBot.js
+++ MonPremBot.js
@@ -13,7 +13,7 @@ var restify = require('restify');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+   console.log('%s écoute sur le port %s', server.name, server.url); 
 });
   
 // // Create chat bot
