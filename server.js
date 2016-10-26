41b3166d87fc1bba6a19077c602226942110984c
Eric Vernie <ericv@microsoft.com>
2016-10-26 16:49:25 +0200

    merging step2

--- server.js
+++ server.js
@@ -1,40 +1,20 @@
 var builder = require("botbuilder");
 var restify = require('restify');
-//Step 1
-// var connector = new builder.ConsoleConnector().listen();
-// var bot = new builder.UniversalBot(connector);
 
-// bot.dialog('/',function(session) {    
-//     session.send("Vous avez dit : " + session.message.text);
-// });
-
-
-//Step 2
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s ecoute sur to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// // Create chat bot
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// //=========================================================
-// // Bots Dialogs
-// //=========================================================
-
-bot.dialog('/',function (session) {
-    session.send("Bienvenue dans cette démonstration NodeJs Bot");    
+bot.dialog('/',function(session) {    
+    session.send("Vous avez dit : " + session.message.text);
 });
-  
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
